66ead56fb23b4607efa979b2039cafb8a01611c3
Akindeju Oluwagbemiga Michael <akindejuhmikel@gmail.com>
2024-07-07 16:38:06 +0100

    fix: auth loading

--- src/context/auth/auth.tsx
+++ src/context/auth/auth.tsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext, IAuthProvider } from './interfaces';
 
 export const AuthProvider: IAuthProvider = function AuthProvider({ children }) {
+  const [render, setRender] = useState<boolean>(false);
   const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    setRender(true);
+  }, []);
 
   return (
     <AuthContext.Provider
@@ -10,7 +15,7 @@ export const AuthProvider: IAuthProvider = function AuthProvider({ children }) {
         user,
         setUser,
       }}>
-      {children}
+      {render ? children : null}
     </AuthContext.Provider>
   );
 };
